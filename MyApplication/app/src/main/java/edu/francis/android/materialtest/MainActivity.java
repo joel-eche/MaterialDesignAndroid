14b3965f2f7cd89c4ba736625d8d04151f5ce9f1
Joel Eche <joel.eche5@hotmail.com>
2016-11-12 23:49:01 -0500

    Add Toolbar

--- MyApplication/app/src/main/java/edu/francis/android/materialtest/MainActivity.java
+++ MyApplication/app/src/main/java/edu/francis/android/materialtest/MainActivity.java
@@ -2,12 +2,36 @@ package edu.francis.android.materialtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+//import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar=(Toolbar)findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id=item.getItemId();
+        Toast.makeText(this,"Holo "+item.getTitle(),Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
+    }
+
 }
